01a2110721ad6524569e11a4a15e2a64638585e0
rodges@gmail.com <rodges@gmail>
2015-11-06 21:45:57 -0200

    Um verde e um vermelho

--- clinica-veterinaria-baade/src/test/java/AtendimentoRotinaTeste.java
+++ clinica-veterinaria-baade/src/test/java/AtendimentoRotinaTeste.java
@@ -5,7 +5,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.baade.*;
-public class teste {
+public class AtendimentoRotinaTeste {
 Cliente cliente = new Cliente();
 
 Animal animal = new Animal();
